87f2317659d241bfbac558ab1eba02fa9549a1b1
zhazaira <zmahanzazira@gmail.com>
2023-12-01 19:18:11 +0600

    backend

--- programming/models.py
+++ programming/models.py
@@ -4,3 +4,11 @@ from django.db import models
 class Programming(models.Model):
     language= models.CharField(max_length=255)
     since = models.IntegerField()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
--- programming/tests.py
+++ programming/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+
+class TestPostModel(TestCase):
+
+    def test_post_has_title(self):
+        post = Post.objects.create(title='Тестовый пост', content='Содержание тестового поста')
+        self.assertEqual(post.title, 'Тестовый пост')
+
+    def test_post_has_content(self):
+        post = Post.objects.create(title='Тестовый пост', content='Содержание тестового поста')
+        self.assertEqual(post.content, 'Содержание тестового поста')
+
+    def test_post_str_method(self):
+        post = Post.objects.create(title='Тестовый пост', content='Содержание тестового поста')
+        self.assertEqual(str(post), 'Тестовый пост')
